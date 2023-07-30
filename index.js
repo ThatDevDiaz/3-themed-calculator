37f075e6fa36ebe54d8f7da629d009444120b2ca
Cesar Diaz <dev.cdiaz@gmail.com>
2023-07-30 10:40:46 -0500

    initial build

---
+++ index.js
@@ -0,0 +1,6 @@
+// Receive and remember user input
+// Calculate chosen expression with user's input
+// Return results into the display and store results
+// Repeat prior steps starting at 1 with stored data until reset button is used
+// Reset calculator
+// Add multiple color themes
