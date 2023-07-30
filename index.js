438688175c45bcfc3ce7f46a161c6aa0ee48daf8
Cesar Diaz <dev.cdiaz@gmail.com>
2023-07-30 16:12:24 -0500

    js start

--- index.js
+++ index.js
@@ -4,3 +4,22 @@
 // Repeat prior steps starting at 1 with stored data until reset button is used
 // Reset calculator
 // Add multiple color themes
+
+const one = document.getElementById(`btn1`);
+const two = document.getElementById(`btn2`);
+const three = document.getElementById(`btn3`);
+const four = document.getElementById(`btn4`);
+const five = document.getElementById(`btn5`);
+const six = document.getElementById(`btn6`);
+const seven = document.getElementById(`btn7`);
+const eight = document.getElementById(`btn8`);
+const nine = document.getElementById(`btn9`);
+const zero = document.getElementById(`btn0`);
+const del = document.getElementById(`btnDel`);
+const plus = document.getElementById(`btnPlus`);
+const minus = document.getElementById(`btnMinus`);
+const divide = document.getElementById(`btnDivide`);
+const multiply = document.getElementById(`btnMultiply`);
+const point = document.getElementById(`btnPoint`);
+const reset = document.getElementById(`btnReset`);
+const equals = document.getElementById(`btnEquals`);
